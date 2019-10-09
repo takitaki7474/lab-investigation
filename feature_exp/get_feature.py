5a564e944e007811bd447b2d1c69a3d539edf6ff
takitaki7474 <shark747474@gmail.com>
2019-10-09 19:28:06 +0900

    Improve get_feature() on gpu

--- feature_exp/get_feature.py
+++ feature_exp/get_feature.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 import time
 import chainer
 from chainer.links import VGG16Layers
@@ -21,9 +22,12 @@ def extract_feature(dataset, model, layer="fc7"):
         feature = model.extract([tensor], layers=[layer])[layer].data
         l_img_name.append(img_name)
         l_feature.append(feature)
-        l_label.append(l_label)
+        l_label.append(label)
 
         data_count += 1
+
+        if data_count % 1000 == 0:
+            print(data_count)
 
     f_table["img_name"] = l_img_name
     f_table["feature"] = l_feature
@@ -33,10 +37,32 @@ def extract_feature(dataset, model, layer="fc7"):
     return df
 
 
+def save_df(df, pkl="f_table.pkl"):
+
+    if not os.path.exists("./feature"):
+        os.mkdir("./feature")
+
+    path = os.path.join("./feature", pkl)
+    df.to_pickle(path)
+
+
 if __name__ == "__main__":
+
+    device = 0 #cpu#device=-1
+
     train, test = chainer.datasets.get_cifar10()
     model = VGG16Layers()
+
+    if device >= 0:
+    	import cupy as cp
+    	import chainer.cuda
+
+    	model.to_gpu()
+    else:
+    	cp = np
 
     start = time.time()
     df = extract_feature(train, model)
     print("抽出時間 {}秒".format(time.time()-start))
+
+    save_df(df)
